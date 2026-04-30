0de47869e333c9e5b69e30e6a3c930296495930b
SpSf2 <kiworks.duglas@gmail.com>
2026-04-30 10:42:08 +0200

    Uso avanzado del Enum Explicado

--- el-tipo-enum-avanzado/src/main/java/com/example/App.java
+++ el-tipo-enum-avanzado/src/main/java/com/example/App.java
@@ -5,32 +5,14 @@ import java.util.stream.Stream;
 
 public class App {
     public static void main(String[] args) {
-        /*  El enum tiene un método que se llama Values() que permite iterar a través de las constantes del enum y mostrar
-         * el ordinal asociado a dicha constante y el nombre de tipo String de la propia constante  */
     	
-    	// Primero utilizando for mejorado
+    	//  Frecuencia de visitantes al Zoo en Spring:
     	
-    	for (Season season : Season.values()) {
-    		
-    		System.out.println("Nombre de la Constante: " + season.name() + ", ordinal: " + season.ordinal());
+    	Season.SPRING.printExpectedVisitors();
     	
-			
-		}
-    	//  Segundo: utilizando operaciones de agregado
+    	//  Horario de isitas en Verano:
     	
-    	// asi se podría: new ArrayList<Season>(Arrays.asList(Season.values())); pero el siguiente es mejor
-    	// Arrays.stream(Season.values());  esta es la mejor
-    	// y así se crea una variable de flujo:
-    /*	Stream<Season> flujoDeSeason = Arrays.stream(Season.values());
-    	
-    	flujoDeSeason.forEach(season -> System.out.println("Nombre: " + season.name()
-    			+ ", Ordinal: " + season.ordinal()));
-    	*/
-    	/* La variable flujoDeSeason es redundante, porque solamente sirve para cuando uno es un principiante,
-    	 * entender que el método String de la clase Arrays genera un flujo (Stream) a partir de un Array. */
-    	
-    	Arrays.stream(Season.values())
-    	.forEach(season -> System.out.println("Nombre: " + season.name()
-		+ ", Ordinal: " + season.ordinal()));
+    	System.out.println("El Horario de visitas al Zoo en Verano es: " + Season.SUMMER.getHours());
+
     }
 }
--- el-tipo-enum-avanzado/src/main/java/com/example/Season.java
+++ el-tipo-enum-avanzado/src/main/java/com/example/Season.java
@@ -1,6 +1,70 @@
+/* 
+* Los enumerables, al igual que las clases, tambien pueden contener
+* constructores, campos (propiedades o atributos) y metodos */
+
+/*  A modo de ejemplo:  Un Zoo ficticio que quieren conocer la frecuencia o afluencia de visitantes 
+ *  segun la epoca del año (es alta, media o baja*/
+
+/* Cuando un enum va a contener algo más que las constantes, todo tiene que ir después de las 
+ * constantes, separado por punto y coma ( ; ) */
+
 package com.example;
 
 public enum Season {
-	SPRING, SUMMER, WINTER, FALL
+	SPRING("La Afluencia de visitantes en Primavera es: Media") {
+		@Override
+		public String getHours() {
+			// TODO Auto-generated method stub
+			return "De 9:00 a.m.  a   17:00 p.m"; // SE PUEDE COLOCAR AQUI TODO EL PRINT COMO SE HIZO EN 
+		}                                         // LA CONSTANTE
+	}, 
+	WINTER("La Afluencia de visitantes en Invierno es: Baja") {
+		@Override
+		public String getHours() {
+			// TODO Auto-generated method stub
+			return "De 10:00 a.m.  a   15:00 p.m";
+		}
+	}, 
+	SUMMER("La Afluencia de visitantes en Verano es: Alta") {
+		@Override
+		public String getHours() {
+			
+			// TODO Auto-generated method stub
+			return "De 9:00 a.m.  a   19:00 p.m";
+		}
+	}, 
+	FALL("La Afluencia de visitantes en Otoño es: Media") {
+		@Override
+		public String getHours() {
+			// TODO Auto-generated method stub
+			return "De 9:00 a.m.  a   17:00 p.m";
+		}
+	};
+	
+	private final String expectedVisitors;
+	
+	private Season(String expectedVisitor) {
+		this.expectedVisitors = expectedVisitor;
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void printExpectedVisitors()  {
+		System.out.println(expectedVisitors);
+	}
+	//  Establecer el horario de visitas al Zoo según la época del año
+	
+    public abstract String getHours();
+    
+	
 }
+
+
+
+
+
+
+
+
+
+
 
